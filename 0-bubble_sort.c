bd0a871fa05f77875fd7e6e3caea3279d31fd72f
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2023-04-24 16:08:14 +0000

    sorting algorithim

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,38 +1,36 @@
 #include "sort.h"
 
 /**
- * swap - Function to swap two integers
- * @a: Integer 1
- * @b: Integer 2
- */
-void swap(int a, int b)
-{
-	int tmp;
-
-	tmp = a;
-	a = b;
-	b = tmp;
-}
-
-/**
- * bubble_sort - Function to bubble sort an array of size n
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
  * @array: The array to be sorted
  * @size: The size of the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, n, new_n;
+	int swap;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
-		for (j = 0; j < size - i - 1; j++)
+	n = size;
+	while (n > 0)
+	{
+		new_n = 0;
+		for (i = 0; i < n - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap(array[i], array[i + 1]);
+				swap = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = swap;
+				new_n = i + 1;
 				print_array(array, size);
 			}
 		}
+		n = new_n;
+	}
 }
